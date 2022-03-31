caec7aa6776c4c9a55e2e8733bfc3c6cf267d528
fayweb <fy.webster@gmail.com>
2022-03-31 15:16:46 +0200

    working on the heatmap script for the facs data

--- r_scripts/02_input2product/01_pheatmap_facs_lab_script.R
+++ r_scripts/02_input2product/01_pheatmap_facs_lab_script.R
@@ -7,11 +7,52 @@ library(tibble)
 
 ### Import the data 
 
+### Import the data 
 facs_lab <- read.csv("https://raw.githubusercontent.com/fayweb/Eimeria_mouse_immunity/main/data_products/01_intermediate_files/01_lab_facs_mLN")
 
+# Here is a vector to select the cell count columns
+CellCount.cols <- c("CD4", "Treg", "Div_Treg", "Treg17", "Th1", "Div_Th1", "Th17", 
+                    "Div_Th17", "CD8", "Act_CD8", "Div_Act_CD8", "IFNy_CD4", 
+                    "IFNy_CD8","Treg_prop", "IL17A_CD4")
 
 ### Prepare the annotation data frame 
 
-annotation_df <- fac %>% select(c("EH_ID", "primary_infection", 
-                                     "challenge_infection", "infection_history",
-                                     "mouse_strain", "relative_weight"))
+### Prepare the annotation data frame for the heatmap
+annotation_df <- facs_lab %>% 
+  select(c("EH_ID", "primary_infection", "challenge_infection", "infection_history",
+           "mouse_strain", "max_weight_loss"))
+
+### Data tidying for the heatmap function
+facs <- facs_lab  %>% 
+  select(c(EH_ID, CellCount.cols))
+
+# turn the data frame into a matrix and transpose it. We want to have each cell 
+# type as a row name 
+facs <- t(as.matrix(facs))
+
+#switch the matrix back to a data frame format
+facs <- as.data.frame(facs)
+
+ # turn the first row into column names
+facs %>%
+  row_to_names(row_number = 1) -> facs
+
+# Now further prepare the data frame for plotting by removing the first row
+ ## and convert the column to row names with the cells 
+facs[-1, ] -> heatmap_data
+
+ # How do our data look like?
+  glimpse(heatmap_data) #alll columns appear to be numeric
+
+# turn the columns to numeric other wise the heatmap function will not work
+heatmap_data[] <- lapply(heatmap_data, function(x) as.numeric(as.character(x)))
+
+### Prepare the annotation columns for the heatmap
+rownames(annotation_df) <- annotation_df$EH_ID
+
+# Match the row names to the heatmap data frame
+rownames(annotation_df) <- colnames(facs)
+
+ heatmap_data %>% 
+   pheatmap(annotation_col = annotation_df, scale = "row")
+
