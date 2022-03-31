546610e0447015716bf605bd5ba3adecb2757df1
fayweb <fy.webster@gmail.com>
2022-03-31 12:11:50 +0200

    starting the heat map script for the facs data

--- r_scripts/02_input2product/01_pheatmap_facs_lab_script.R
+++ r_scripts/02_input2product/01_pheatmap_facs_lab_script.R
@@ -4,3 +4,14 @@ library(matrixStats)
 library(tidyr)
 library(janitor)
 library(tibble)
+
+### Import the data 
+
+facs_lab <- read.csv("https://raw.githubusercontent.com/fayweb/Eimeria_mouse_immunity/main/data_products/01_intermediate_files/01_lab_facs_mLN")
+
+
+### Prepare the annotation data frame 
+
+annotation_df <- fac %>% select(c("EH_ID", "primary_infection", 
+                                     "challenge_infection", "infection_history",
+                                     "mouse_strain", "relative_weight"))
