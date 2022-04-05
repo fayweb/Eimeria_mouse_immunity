66f702d41151ee70d04a44b3f1d481866bc51136
fayweb <fy.webster@gmail.com>
2022-04-05 11:03:52 +0200

    edited and brought the script to work

--- r_scripts/gene_expression/01_gene_pheatmap.R
+++ r_scripts/gene_expression/01_gene_pheatmap.R
@@ -5,94 +5,60 @@ library(tidyr)
 library(janitor)
 library(tibble)
 
+
 ### Import the data
 Challenge <- read.csv("https://raw.githubusercontent.com/derele/Eimeria_Lab/master/data_products/Challenge_infections.csv")
 
-#cor(Challenge[, grepl("_N", colnames(Challenge))], use="pairwise.complete.obs")
-
-
-##  select(ends_with("_N")) those are the gene expression column
-### select the columns you want to plot
-##  filter(if_any(everything(), ~ !is.na(.)))%>% ## remove rows only NA
-##  summarise_each(funs(sum(is.na(.)))) ### remove columns only NA
-##.... 
-##... 
-##pheatmap() ## do the pheatmap
-
-
-gene <- as_tibble(Challenge) %>%
-  filter(infection == "challenge", dpi == "8") %>%
-  group_by(EH_ID, infection) %>%
-  select(c("EH_ID", "primary_infection", "challenge_infection", "infection_history",
+basics_gene <- as_tibble(Challenge) %>%
+  dplyr::filter(infection == "challenge", dpi == "8") %>%
+  dplyr::group_by(EH_ID, infection) %>%
+  dplyr::select(c("EH_ID", "primary_infection", "challenge_infection", "infection_history",
            "mouse_strain", "max_WL", ends_with("_N"))) 
 
-  
-  summarize(max_OOC = max(OOC, na.rm=TRUE),
-            max_WL = min(relative_weight, na.rm=TRUE),
-            experiment = unique(experiment),
-            mouse_strain= unique(mouse_strain),
-            primary_infection=unique(primary_infection),
-            challenge_infection=unique(challenge_infection),
-            infection_history=unique(infection_history),
-            infection_type=unique(infection_type),
-            experiment=unique(experiment)) -> %>% 
-  
-  
-  
-  filter(infection == "challenge") %>%
-  group_by("EH_ID") %>%
-  select(c("EH_ID", "primary_infection", "challenge_infection", "infection_history",
-           "mouse_strain", "max_WL", ends_with("_N")))
+# remove duplicated and change the format to data frame 
+basics_gene <- unique(basics_gene) %>% as.data.frame(basics_gene) %>% select(-infection)
+ 
+gene <- basics_gene %>% dplyr::select(c(EH_ID, ends_with("_N")))
 
+ 
+ # turn the data frame into a matrix and transpose it. We want to have each cell 
+ # type as a row name 
+ gene <- t(as.matrix(gene))
+ 
+ #switch the matrix back to a data frame format
+ gene <- as.data.frame(gene)
+ 
+ # turn the first row into column names
+ gene %>%
+     row_to_names(row_number = 1) -> gene
+ 
+ # Now further prepare the data frame for plotting by removing the first row
+ ## and convert the column to row names with the cells 
+ gene[-1, ] -> heatmap_data
+ 
+ 
+ table(rowSums(is.na(heatmap_data)) == nrow(heatmap_data))
+ 
 
-  
-  Filter(function(x)!all(is.na(x)), gene)
+ 
+ # How do our data look like?
+ glimpse(heatmap_data) #alll columns appear to be numeric
+ 
+ # turn the columns to numeric other wise the heatmap function will not work
+ heatmap_data[] <- lapply(heatmap_data, function(x) as.numeric(as.character(x)))
 
-#%>%
-  summarise_each(funs(sum(is.na(.))))
+ # remove columns with only NAs 
+ heatmap_data <- Filter(function(x)!all(is.na(x)), heatmap_data) 
+ 
+ #remove rows with only Nas
+ heatmap_data <-  heatmap_data[, colSums(is.na(heatmap_data)) != nrow(heatmap_data)]
+ 
 
-
-#%>%
-  filter(if_any(everything(), ~ !is.na(.)))
-
-         
-         ### Prepare the annotation data frame for the heatmap
 ### Prepare the annotation data frame for the heatmap
-annotation_df <- FACS %>% 
-  filter(infection == "challenge") %>%
+annotation_df <- basics_gene %>%
   select(c("EH_ID", "primary_infection", "challenge_infection", "infection_history",
            "mouse_strain", "max_WL"))
 
-#missing from the annotation columns 
-#"max_weight_loss"
-### Data tidying for the heatmap function
-gene <- gene_lab  %>% 
-  select(c(EH_ID, Gene.Exp.cols.norm ))
-
-# turn the data frame into a matrix and transpose it. We want to have each cell 
-# type as a row name 
-gene <- t(as.matrix(gene))
-
-#switch the matrix back to a data frame format
-gene <- as.data.frame(gene)
-
-# turn the first row into column names
-gene %>%
-  row_to_names(row_number = 1) -> gene
-
-# Now further prepare the data frame for plotting by removing the first row
-## and convert the column to row names with the cells 
-gene[-1, ] -> heatmap_data
-
-
-table(rowSums(is.na(heatmap_data))==nrow(heatmap_data))
-
-#hhh
-# How do our data look like?
-glimpse(heatmap_data) #alll columns appear to be numeric
-
-# turn the columns to numeric other wise the heatmap function will not work
-heatmap_data[] <- lapply(heatmap_data, function(x) as.numeric(as.character(x)))
 
 annotation_df <- as.data.frame(annotation_df)
 
@@ -100,22 +66,22 @@ annotation_df <- as.data.frame(annotation_df)
 rownames(annotation_df) <- annotation_df$EH_ID
 
 # Match the row names to the heatmap data frame
-rownames(annotation_df) <- colnames(gene)
+rownames(annotation_df) <- colnames(basics_gene)
 
 #remove the unecessary column
 annotation_df <- annotation_df %>% select(-EH_ID, )
 
-heatmap_data2 <- heatmap_data[rowSums(is.na(heatmap_data))<41,
-                              colSums(is.na(heatmap_data))<6]
-
+#heatmap_data2 <- heatmap_data[rowSums(is.na(heatmap_data))<41,
+                             # colSums(is.na(heatmap_data))<6]
 
 #plot the heatmap
-jpeg("data_products/02_output_files/02_plots/Pheatmap_gene_lab.jpg", width = 800, height = 1000)
+jpeg("output_data/gene_expression/Pheatmap_gene_lab.jpg", width = 1400, height = 1000)
 
-pheatmap(heatmap_data2, annotation_col = annotation_df, scale = "row")
+pheatmap(heatmap_data, annotation_col = annotation_df, scale = "row")
 
 
 #close the jpeg file
 dev.off()
 
-#test 4
+#switch off all dev devices
+while (!is.null(dev.list()))  dev.off()
