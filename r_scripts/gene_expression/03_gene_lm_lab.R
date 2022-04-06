82bfeb0a838de4190ec4bcaae32f92cb91c97220
fayweb <fy.webster@gmail.com>
2022-04-06 16:29:40 +0200

    plots gene expression vs intensity, weight change vs intensity

--- r_scripts/gene_expression/03_gene_lm_lab.R
+++ r_scripts/gene_expression/03_gene_lm_lab.R
@@ -1,9 +1,46 @@
 library(ggplot2)
+library(tidyr)
+library(dplyr)
 
 source("r_scripts/gene_expression/02_gene_correlation_matrix.R")
 
-gene_intensity <- gene_na_omit %>% select(c("EH_ID", "delta", ends_with("_N"))) %>%
-  pivot_longer(cols = 3:23, names_to = "Gene", values_to = "expression")
+### Remove unecssary files from the environment
+rm(annotation_df, basics_gene, gene, heatmap_data, gene)
+
+# Plotting weight against each dpi of infection, dividing into different species 
+
+#switch off all dev devices
+while (!is.null(dev.list()))  dev.off() 
+
+jpeg("output_data/gene_expression/04_weight_dpi.jpg", width = 1400, height = 1000)
+
+Challenge %>% 
+  filter(infection == "challenge", !dpi %in% "0") %>%
+  drop_na(weight_dpi0, relative_weight) %>%
+  group_by("EH_ID") %>%
+  ggplot(aes(x = dpi, y = relative_weight, color = challenge_infection)) +
+  geom_jitter() +
+  stat_smooth()
+
+dev.off()
+
+# Now plot the gene expression agains infection intensity
+gene_expr_delta <- gene_na_omit %>%
+  pivot_longer(cols = 8:28, names_to = "Gene", values_to = "gene_expression") %>%
+  na.omit(expression) %>%
+  ggplot(aes(x = delta, y = gene_expression, color = challenge_infection))
+
+jpeg("output_data/gene_expression/05_gene_expression_intensity", width = 800, height = 600)
+
+gene_expr_delta +
+  geom_jitter() +
+  facet_wrap(~ Gene, scales = "free") +
+  theme_bw()
+
+dev.off()
+
+ggplot(data = gene_intensity, aes(x = c))
+
 
 ggplot(gene_na_omit, aes(ends_with("_N")))
 
